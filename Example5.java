dedf7218c2be9896192b2866bbcb8232627ec8bc
Dhumalgayatri <89679091+Dhumalgayatri@users.noreply.github.com>
2021-08-28 20:39:07 +0530

    Add files via upload

---
+++ Example5.java
@@ -0,0 +1,18 @@
+//JAVA program to calculate the volume of a cube//
+
+
+import java.util.*;
+import java.lang.Math.*;
+public class Cubev {
+	public static void main(String[] args) {
+	Scanner sc=new Scanner (System.in);
+	System.out.println("Calculate the Volume of a cube");
+	System.out.println("----------------------------------------------------------------------------");
+	System.out.println("Input the side of cube:");
+	int side=sc.nextInt();
+	int volume;
+	volume=side*side*side;
+	System.out.println("The volume of a cube is:");
+		
+	}
+}
