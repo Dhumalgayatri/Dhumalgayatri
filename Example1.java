0a6b5041761937267df997103a485593226bf1ee
Dhumalgayatri <89679091+Dhumalgayatri@users.noreply.github.com>
2021-08-28 11:35:46 +0530

    Add files via upload

---
+++ Example1.java
@@ -0,0 +1,16 @@
+//java program to print the result of the specified operatins//
+
+
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println("Result of 1 st expression is:");
+		System.out.println(-1+4*6);
+		System.out.println("Result of 2nd expression:");
+		System.out.println((35+5)%7);
+		System.out.println("Result of 3rd expression:");
+		System.out.println(14+(-4)*6/11);
+    	System.out.println("Result of 4th expression:");
+    	System.out.println(2+15/6*1-7%2);
+	}
+}
