b39c536d6558b6898db5bc86c22f5e9b3731be5e
Dhumalgayatri <89679091+Dhumalgayatri@users.noreply.github.com>
2021-08-29 14:24:06 +0530

    Add files via upload

---
+++ Ex2.java
@@ -0,0 +1,23 @@
+//Write a program in java to print the sum of two numbers
+
+
+public class SumOfNumbers {
+	public static void main(String[] args) {
+		System.out.println("print the sum of two numbers:");
+		System.out.println("----------------------------------------------------");
+	
+		int a=20;
+     int b=30;
+     int sum= a+b;
+  
+  System.out.println("sum of "+a+ " and "+ b+ " is: "+sum);
+ 
+	}
+}
+
+//OUTPUT:
+//print the sum of two numbers:
+//----------------------------------------------------
+//sum of 20 and 30 is: 50
+
+//[Program finished]
